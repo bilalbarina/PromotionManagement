fea406ea1b151d3b002b043ebba3a58378406ae8
bilalbarina <dev.barina@gmail.com>
2022-10-11 11:11:42 +0100

    Updated database stracture

--- Data/database.sql
+++ Data/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2022 at 12:09 PM
+-- Generation Time: Oct 11, 2022 at 12:11 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `promotions` (
   `id` int(11) NOT NULL,
-  `title` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
