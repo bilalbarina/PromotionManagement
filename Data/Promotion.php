32001d6110ec9b51ca3783d46870b3306a62f5b9
bilalbarina <dev.barina@gmail.com>
2022-10-11 13:05:25 +0100

    Prototype BASIC

--- Data/Promotion.php
+++ Data/Promotion.php
@@ -6,18 +6,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 class Promotion {
 
+    // Create new promotion
     public function create($title)
     {
         $query = "INSERT INTO promotions (title) VALUES('$title')";
         return mysqli_query(connection(), $query);
     }
 
+    // Update existing promotion
     public function update($id, $title) 
     {
         $query = "UPDATE promotions SET title = '$title' WHERE id = '$id'";
         return mysqli_query(connection(), $query);
     }
 
+    // Get promotion as an object
     public function get($id)
     {
         $query = "SELECT * FROM promotions WHERE id = '$id'";
@@ -30,5 +33,12 @@ class Promotion {
         // return $promotion;
 
         return (object) $assoc;
+    }
+
+    // Delete a promotion
+    public function delete($id)
+    {
+        $query = "DELETE FROM promotions WHERE id = '$id'";
+        return mysqli_query(connection(), $query);
     }
 }
