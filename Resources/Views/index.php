24f8fc8100e1428f28c2c93f88b8cee20c5fb0fd
bilalbarina <dev.barina@gmail.com>
2022-10-11 16:44:55 +0100

    Prototype BASIC

--- Resources/Views/index.php
+++ Resources/Views/index.php
@@ -4,11 +4,28 @@ namespace Data;
 
 include __DIR__ . '/../../vendor/autoload.php';
 
-$promotionObject = new Promotion();
-$created = $promotionObject->create('test');
-
-if ($created) {
-    echo 'Promotion created';
-} else {
-    echo 'Promotion not created';
+if (isset($_POST['title'])) {
+    $promotionObject = new Promotion();
+    $created = $promotionObject->create($title);
+    die('Created');    
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Ajouter </title>
+</head>
+<body>
+
+<form action="" method="post">
+    <input type="text" placeholder="Title">
+    <button type="submit"> Ajouter </button>
+</form>
+    
+</body>
+</html>
