5adc0786cb002601800bf9c2539d25294b7eda65
bilalbarina <dev.barina@gmail.com>
2022-10-14 10:14:24 +0100

    Updates

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ session_start();
 
 define('URI', $_SERVER['REQUEST_URI']);
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $page = $_GET['page'] ?? '';
 $pagePath = __DIR__ . '/Resources/Views/' . $page . '.php';
 
 if (file_exists($pagePath)) {
     include $pagePath;
-    die();
+    exit();
 }
 die('Not Found');
 
